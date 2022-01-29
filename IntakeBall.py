2756a56af3c33d97e84f4bd6d202a12c95c81da7
THajiyev <75402235+THajiyev@users.noreply.github.com>
2022-01-28 21:08:04 -0600

    Update IntakeBall.py

--- IntakeBall.py
+++ IntakeBall.py
@@ -4,28 +4,15 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 # cap.set(cv2.CAP_PROP_EXPOSURE,-5)
-def colorFilter(img,low1,high1):
-    blurred = cv2.GaussianBlur(img, (11, 11), 0)
+def colorFilter(img,lower, upper):
+    blurred = cv2.GaussianBlur(img, (9,9), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-# construct a mask for the color "green", then perform
-# a series of dilations and erosions to remove any small
-# blobs left in the mask\
-    lower = np.array([10,100,20])
-    upper = np.array([25,255,255])
-    mask = cv2.inRange(hsv, low1, high1)
-    # mask = cv2.erode(mask, None, iterations=2)
-    # mask = cv2.dilate(mask, None, iterations=2)
-    
-
-           
+    mask = cv2.inRange(hsv, lower, upper) 
     return mask
 
-def getContours(mask, minArea):
-        height, width = mask.shape
+def getContours(mask, minArea=40):
         edges=None
-        edges = cv2.Canny(mask,225/3, 225, apertureSize=5)
-        cv2.imshow("Canny", edges)
+        edges = cv2.Canny(mask,100, 200)
         contours, _= cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         newContours = []
       
@@ -41,20 +28,16 @@ def empty(a):
 # trackbars for adjusting hsv
 cv2.namedWindow("HSV")
 cv2.resizeWindow("HSV", 300, 300)
-cv2.createTrackbar("HUE Min", "HSV", 0, 179, empty)
-cv2.createTrackbar("HUE Max", "HSV", 179, 179, empty)
-cv2.createTrackbar("SAT Min", "HSV", 0, 255, empty)
+cv2.createTrackbar("HUE Min", "HSV", 89, 179, empty)
+cv2.createTrackbar("HUE Max", "HSV", 130, 179, empty)
+cv2.createTrackbar("SAT Min", "HSV", 16, 255, empty)
 cv2.createTrackbar("SAT Max", "HSV", 255, 255, empty)
-cv2.createTrackbar("VALUE Min", "HSV", 0, 255, empty)
+cv2.createTrackbar("VALUE Min", "HSV", 105, 255, empty)
 cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
 
 
 while True:
     success, img = cap.read()
-   
-    
-  
-
     # # read trackbar positions
     h_min = cv2.getTrackbarPos("HUE Min", "HSV")
     h_max = cv2.getTrackbarPos("HUE Max", "HSV")
@@ -72,7 +55,7 @@ while True:
     
 
     cv2.imshow('image', mask)
-    cv2.drawContours(img, getContours(mask,2000), -1, (0,255,0), 3)
+    img = cv2.drawContours(img, getContours(mask), -1, (0,255,0), 3)
     cv2.imshow('normal', img)
     
     
