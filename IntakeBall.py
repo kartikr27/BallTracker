817a7d3947014e3f5f0bdeea3c1c428196678b63
kartikr27 <krishsuper27@gmail.com>
2022-02-04 18:28:34 -0600

    Update IntakeBall.py

--- IntakeBall.py
+++ IntakeBall.py
@@ -2,24 +2,30 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
+cap.set(cv2.CAP_PROP_EXPOSURE,-96/11)
 # cap.set(cv2.CAP_PROP_EXPOSURE,-5)
 def colorFilter(img,lower, upper):
-    blurred = cv2.GaussianBlur(img, (9,9), 0)
+    blurred = cv2.GaussianBlur(img, (3,3), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower, upper) 
     return mask
 
 def getContours(mask, minArea=40):
-        edges=None
+        
         edges = cv2.Canny(mask,100, 200)
         contours, _= cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         newContours = []
       
         for contour in contours:
-            if cv2.contourArea(contour)>minArea:
+            approx = cv2.approxPolyDP(contour, .03*cv2.arcLength(contour, True), True)
+
+            if cv2.contourArea(contour)>minArea and len(approx)>8:
                     newContours.append(contour)
-        return newContours
+        sortedContoursByArea=sorted(newContours, key=cv2.contourArea, reverse=False)
+        if len(newContours)!=0:
+            return [newContours[0]]
+        return []
 
 def empty(a):
     pass
@@ -28,11 +34,11 @@ def empty(a):
 # trackbars for adjusting hsv
 cv2.namedWindow("HSV")
 cv2.resizeWindow("HSV", 300, 300)
-cv2.createTrackbar("HUE Min", "HSV", 89, 179, empty)
-cv2.createTrackbar("HUE Max", "HSV", 130, 179, empty)
-cv2.createTrackbar("SAT Min", "HSV", 16, 255, empty)
+cv2.createTrackbar("HUE Min", "HSV", 100, 179, empty)
+cv2.createTrackbar("HUE Max", "HSV", 140, 179, empty)
+cv2.createTrackbar("SAT Min", "HSV", 150, 255, empty)
 cv2.createTrackbar("SAT Max", "HSV", 255, 255, empty)
-cv2.createTrackbar("VALUE Min", "HSV", 105, 255, empty)
+cv2.createTrackbar("VALUE Min", "HSV", 0, 255, empty)
 cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
 
 
@@ -55,6 +61,7 @@ while True:
     
 
     cv2.imshow('image', mask)
+    
     img = cv2.drawContours(img, getContours(mask), -1, (0,255,0), 3)
     cv2.imshow('normal', img)
     
